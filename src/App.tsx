1a5b88ea91995174708785db857afaf92b36d5bf
sebastien-violante <sebastien.violante@gmail.com>
2026-09-11 17:52:14 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+ <h1>Sébastien VIOLANTE</h1>
+    </>
+  )
+}
+
+export default App
