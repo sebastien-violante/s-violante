24fbfe4668457951315c40ac8f4899c2dedfa9e4
sebastien-violante <sebastien.violante@gmail.com>
2026-09-12 12:29:49 +0200

    création ratelier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
-import './App.css'
+import "./App.css";
+import data from "./data/experiences.json"
+import Card from './components/Card/Card'
 
 function App() {
-
   return (
-    <>
- <h1>Sébastien VIOLANTE</h1>
-    </>
-  )
+    <main>
+      <section className="mainData">
+        <h1>Sébastien VIOLANTE</h1>
+        <h2>Concepteur d'application</h2>
+        <h3>sebastien.violante@gmail.com</h3>
+
+        <a href="https://linkedin.com/in/sebastien-violante" target="_blank">
+          <img className="icon" src="/linkedin.png" />
+        </a>
+      </section>
+
+      <section className="ratelier">
+        <button className="mainBulle">
+          <h3>Expérience professionnelle développeur</h3>
+        </button>
+         <button className="mainBulle">
+          <h3>Expérience professionnelle aronautique</h3>
+        </button>
+         <button className="mainBulle">
+          <h3>Centres d'inétrêt</h3>
+        </button>
+      </section>
+        {data.map((experience, index) => (
+          <Card key={index}/>
+        ))}
+      <section>
+
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,14 @@
+import styles from './Card.module.css'
+
+export default function Card() {
+
+    return (
+        <article>
+            <section className={styles.header}>
+                <p>COlor</p>
+            </section>
+        </article>
+            
+        
+    )
+}
